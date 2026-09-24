9bc510476e36d48a46c713359aa8d3822afc875f
Caty <caty.rami.123@gmail.com>
2026-09-24 22:35:30 +0200

    Primer intento

---
+++ PAC1Ex1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PAC1Ex1 {
+
+
+    public static long nextCollatz(long n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+
+        return 3 * n + 1;
+    }
+
+    public static void printCollatzSequence(long n) {
+        System.out.print("Seqüència de Collatz: ");
+
+        do {
+            System.out.print(n + " -> ");
+            n = nextCollatz(n);
+        } while (n >= 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n;
+
+        System.out.print("Introduïu un enter positiu: ");
+        n = scanner.nextLong();
+        printCollatzSequence(n);
+    }
+}
